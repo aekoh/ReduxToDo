f2dc52e917e9c9c1fea1a0a2b04a8998e893bf4d
Aine Ekoh <aineekoh@Aines-MacBook-Pro.local>
2020-10-08 14:01:39 -0500

    redux provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Wrap App component with Provider
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={configureStore()}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
